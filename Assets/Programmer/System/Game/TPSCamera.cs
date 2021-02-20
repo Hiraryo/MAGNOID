a91c682cd66539355e9c964386cbc8f63c776179
Hiraryo <nintendo640804@gmail.com>
2021-02-20 12:03:25 +0900

    Modified move the TPSviewpoint with the mouse XY

--- Assets/Programmer/System/Game/TPSCamera.cs
+++ Assets/Programmer/System/Game/TPSCamera.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TPSCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform _player;
+    private Quaternion _hRotation,_vRotation;
     void Start()
     {
-        
+        _hRotation = Quaternion.identity;
+        _vRotation = Quaternion.identity;
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // Move the mouse X to rotate the camera Y
+        _hRotation *= Quaternion.Euler(0,Input.GetAxis("Mouse X"),0);
+        // Move the mouse Y to rotate the camera X
+        _vRotation *= Quaternion.Euler(Input.GetAxis("Mouse Y"),0,0);
+        // Rotate the camera
+        transform.rotation = _hRotation * _vRotation;
     }
 }
