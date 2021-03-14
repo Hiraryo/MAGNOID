9a7dd823c8228eca246414ede2d97a0b469f410a
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-03-14 18:11:40 +0900

    3/14 progress

--- Assets/Script/Player/Controller.cs
+++ Assets/Script/Player/Controller.cs
@@ -6,11 +6,13 @@ public class Controller
 {
     private void Update()
     {
+        /*
         if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){ PlayerMover(); }
         if (Input.GetButtonDown("Jump") && _Grounded) { JumpMethod(); }
         if (Input.GetButtonDown("NormalAttack")) { PlayerAttacker(0); }
         if (Input.GetButtonDown("HardAttack")) { PlayerAttacker(1); }
         if (Input.GetKeyDown(KeyCode.Return)) { CameraChange(); }
         Model.PlayerJumper();
+        */
     }
 }
