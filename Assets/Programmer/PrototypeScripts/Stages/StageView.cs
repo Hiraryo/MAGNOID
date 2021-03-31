165a3a33a4037f3d4c49b24798823598621f3195
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-03-31 20:36:29 +0900

    Created a program that changes the environment light

---
+++ Assets/Programmer/PrototypeScripts/Stages/StageView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class StageView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// 環境光の種類
+    /// </summary>
+    public enum EnvironmentLight
+    {
+        Morning,Noon,Night
+    }
+    static EnvironmentLight[] EnvLight = { EnvironmentLight.Morning, EnvironmentLight.Noon, EnvironmentLight.Night };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void Lighting(int Date)
+    {
+
+    }
+}
