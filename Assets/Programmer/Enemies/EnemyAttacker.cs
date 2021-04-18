d64776f1f2ec7a4c1794fd0904b4ec98ddbc8b1a
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-04-18 16:31:53 +0900

    Modified enemy program

--- Assets/Programmer/Enemies/EnemyAttacker.cs
+++ Assets/Programmer/Enemies/EnemyAttacker.cs
@@ -5,13 +5,42 @@ namespace Enemies
     interface IEnemyAttacker
     {
         void Attack();
+        void Level();
+        void Status();
     }
 
     class SpiderAttacker : IEnemyAttacker
     {
+        /// <summary>
+        /// 蜘蛛の攻撃スタイルを決めるプログラム（近距離・中距離・遠距離）
+        /// </summary>
         public void Attack()
         {
-            //蜘蛛の攻撃プログラム
+            
+        }
+
+        /// <summary>
+        /// 蜘蛛のレベルを決めるプログラム
+        /// </summary>
+        public void Level()
+        {
+            
+        }
+
+        /// <summary>
+        /// 蜘蛛の状態を決めるプログラム(活発かなど)
+        /// </summary>
+        public void Status()
+        {
+
+        }
+
+        /// <summary>
+        /// 糸を出して攻撃する
+        /// </summary>
+        private void AttackYarn()
+        {
+
         }
     }
 
--- Assets/Programmer/Enemies/EnemyMover.cs
+++ Assets/Programmer/Enemies/EnemyMover.cs
@@ -4,14 +4,53 @@ namespace Enemies
 {
     interface IEnemyMover
     {
-        void Move();
+        void Walk();
+        void Jump();
+        void Fly();
+        void Hide();
+        void Dash();
     }
 
     class SpiderMover : IEnemyMover
     {
-        public void Move()
+        /// <summary>
+        /// 蜘蛛の「歩き」に関するプログラム
+        /// </summary>
+        public void Walk()
         {
-            //蜘蛛を動かすプログラム
+            
+        }
+
+        /// <summary>
+        /// 蜘蛛の「ジャンプ」に関するプログラム
+        /// </summary>
+        public void Jump()
+        {
+
+        }
+
+        /// <summary>
+        /// 蜘蛛の「飛行」に関するプログラム
+        /// </summary>
+        public void Fly()
+        {
+
+        }
+
+        /// <summary>
+        /// 蜘蛛の「透明」に関するプログラム
+        /// </summary>
+        public void Hide()
+        {
+
+        }
+
+        /// <summary>
+        /// 蜘蛛の「ダッシュ」に関するプログラム
+        /// </summary>
+        public void Dash()
+        {
+
         }
     }
 }
