f3c71c9e4d0b6319fe342c5186901827fd6da1a5
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-04-20 06:49:40 +0900

    Corrected the comment

--- Assets/Programmer/Enemies/EnemyBase.cs
+++ Assets/Programmer/Enemies/EnemyBase.cs
@@ -39,7 +39,7 @@ namespace Enemies
         }
 
         /// <summary>
-        /// 蜘蛛の「歩き」に関するプログラム
+        /// 敵の「歩き」に関するプログラム
         /// </summary>
         public void Walk()
         {
@@ -47,7 +47,7 @@ namespace Enemies
         }
 
         /// <summary>
-        /// 蜘蛛の「ジャンプ」に関するプログラム
+        /// 敵の「ジャンプ」に関するプログラム
         /// </summary>
         public void Jump()
         {
@@ -55,7 +55,7 @@ namespace Enemies
         }
 
         /// <summary>
-        /// 蜘蛛の「飛行」に関するプログラム
+        /// 敵の「飛行」に関するプログラム
         /// </summary>
         public void Fly()
         {
@@ -63,7 +63,7 @@ namespace Enemies
         }
 
         /// <summary>
-        /// 蜘蛛の「透明」に関するプログラム
+        /// 敵の「透明」に関するプログラム
         /// </summary>
         public void Hide()
         {
@@ -71,7 +71,7 @@ namespace Enemies
         }
 
         /// <summary>
-        /// 蜘蛛の「ダッシュ」に関するプログラム
+        /// 敵の「ダッシュ」に関するプログラム
         /// </summary>
         public void Dash()
         {
